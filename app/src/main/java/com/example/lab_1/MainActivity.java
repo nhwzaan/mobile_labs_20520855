ee2d70fb1684ade5ff75153bfb97b68cae0edf50
Nguyễn Thị Như Vân <20520855@gm.uit.edu.vn>
2023-04-04 23:02:49 +0700

    commit to lab_1

--- app/src/main/java/com/example/lab_1/MainActivity.java
+++ app/src/main/java/com/example/lab_1/MainActivity.java
@@ -3,12 +3,24 @@ package com.example.lab_1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.GridView;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
-
+    ListView listView;
+    GridView gridView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        listView = findViewById(R.id.customListView);
+        listView.setNestedScrollingEnabled(false);
+        ListViewAdapter customAdapter = new ListViewAdapter(getApplicationContext());
+        listView.setAdapter(customAdapter);
+
+        gridView = findViewById(R.id.gridView);
+        gridView.setNestedScrollingEnabled(false);
+        GridViewAdapter customGridAdapter = new GridViewAdapter(getApplicationContext());
+        gridView.setAdapter(customGridAdapter);
     }
 }
